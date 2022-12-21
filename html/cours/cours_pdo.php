835d43364d1dfbeb2fd8bd4d7793da6552539a64
Kevin Tourret <kevin@drosalys.fr>
2022-12-21 14:46:14 +0100

    exo PDO

--- html/cours/cours_pdo.php
+++ html/cours/cours_pdo.php
@@ -12,16 +12,16 @@ $gameRepository = new GameRepository();
 
 //$gameRepository->createTableExample();
 
-$game = (new Game())
-    ->setName('Pokémon : Ecarlate')
-    ->setSlug('pokemon-ecarlate')
-    ->setPrice(44.0)
-    ->setPublishedAt('2022-11-19 00:00:00')
-    ->setPublisherId(null)
-    ->setDescription('Oui')
-    ->setThumbnailCover('https://www.pokepedia.fr/images/thumb/d/dc/Jaquette_de_Pok%C3%A9mon_%C3%89carlate.png/300px-Jaquette_de_Pok%C3%A9mon_%C3%89carlate.png')
-    ->setThumbnailLogo(null)
-;
-var_dump($game);
-$game = $gameRepository->addGame($game);
-var_dump($game);
+//$game = (new Game())
+//    ->setName('Pokémon : Ecarlate')
+//    ->setSlug('pokemon-ecarlate')
+//    ->setPrice(44.0)
+//    ->setPublishedAt('2022-11-19 00:00:00')
+//    ->setPublisherId(null)
+//    ->setDescription('Oui')
+//    ->setThumbnailCover('https://www.pokepedia.fr/images/thumb/d/dc/Jaquette_de_Pok%C3%A9mon_%C3%89carlate.png/300px-Jaquette_de_Pok%C3%A9mon_%C3%89carlate.png')
+//    ->setThumbnailLogo(null)
+//;
+//var_dump($game);
+//$game = $gameRepository->addGame($game);
+//var_dump($game);
